c8786c9db0dd9eeff979983199f6afa921ca7ced
Louise Vivien Simpelo <louiseviviensimpelo08@gmail.com>
2023-01-08 14:22:10 -0800

    deleted dummy image and modified supplier router

--- router/supplierRoute.js
+++ router/supplierRoute.js
@@ -3,7 +3,7 @@ const multer = require('multer');
 
 const storage = multer.diskStorage({
       destination: function (req, file, callback) {
-        callback(null, "public/warehouse");
+        callback(null, "public/supplier");
       },
       filename: function (req, file, callback) {
         callback(null, Date.now() + "_" + file.originalname);
